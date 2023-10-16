d12f690b7bdabcaf5aba9d4eb5c758d0fbc02ead
Riz <mahmoud.eleresousy@gmail.com>
2023-10-16 15:15:14 +0300

    Add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  # Attributes
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :email, :string
+
+  # Validations
+  validates :first_name, presence: true, length: { maximum: 250 }
+  validates :last_name, presence: true, length: { maximum: 250 }
 end
