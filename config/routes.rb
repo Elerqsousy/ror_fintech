da470b65e57fea216ad1e401843327235325618a
Riz <mahmoud.eleresousy@gmail.com>
2023-10-18 14:30:59 +0300

    Add shrine configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users do
+    resources :groups 
+  end
 
   # authenticated(:user) do
   #   root "dashboard#main"
